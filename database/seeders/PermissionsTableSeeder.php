44c400171bbbdefefda4f370bd8a312dc82eeeed
rizki10132008 <aryantor082@example.com>
2025-01-14 09:32:08 +0700

    membuat seeder user

--- database/seeders/PermissionsTableSeeder.php
+++ database/seeders/PermissionsTableSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class PermissionsTableSeeder extends Seeder
 {
@@ -12,6 +13,26 @@ class PermissionsTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+
+
+        //permission users
+        Permission::create(['name' => 'users index', 'guard_name' => 'web']);
+        Permission::create(['name' => 'users create', 'guard_name' => 'web']);
+        Permission::create(['name' => 'users edit', 'guard_name' => 'web']);
+        Permission::create(['name' => 'users delete', 'guard_name' => 'web']);
+
+        //permission roles
+        Permission::create(['name' => 'roles index', 'guard_name' => 'web']);
+        Permission::create(['name' => 'roles create', 'guard_name' => 'web']);
+        Permission::create(['name' => 'roles edit', 'guard_name' => 'web']);
+        Permission::create(['name' => 'roles delete', 'guard_name' => 'web']);
+
+        //permission permissions
+        Permission::create(['name' => 'permissions index', 'guard_name' => 'web']);
+        Permission::create(['name' => 'permissions create', 'guard_name' => 'web']);
+        Permission::create(['name' => 'permissions edit', 'guard_name' => 'web']);
+        Permission::create(['name' => 'permissions delete', 'guard_name' => 'web']);
+
+       
     }
 }
